b9871f9e6e8c5cb0bfacece6515103524c4a2fd6
ben glumack <hotshotpicts@ben-glumacks-MacBook-Pro.local>
2015-07-19 11:13:44 -0500

    Initial commit

---
+++ public/javascripts/test.js
@@ -0,0 +1,28 @@
+
+var numberGuesses = 3;
+var randomNumber = Math.floor(Math.random() * 10) + 1;
+
+function compareNumber(){   
+ var guess = document.getElementById("guess").value;
+    if(guess > randomNumber){
+       console.log("Your guess is too high!");
+        numberGuesses = numberGuesses - 1;
+
+        if(numberGuesses < 0){
+           console.log("No More Guesses Left")
+        };
+    }
+    else if(guess < randomNumber){
+            console.log("Your guess is too low!");
+            numberGuesses = numberGuesses - 1;
+        if(numberGuesses > 0){
+           console.log("No More Guesses Left")
+            }
+        }
+    else{
+        console.log("Your guess is correct!");   
+    }  
+    
+    console.log(randomNumber); 
+};
+   
