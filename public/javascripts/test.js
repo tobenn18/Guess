97ca3c679f1241d64f679e89d0f031db2d46b5ec
tobenn18 <bglumack@hotmail.com>
2015-07-23 16:02:59 -0500

    Confused validate

--- public/javascripts/test.js
+++ public/javascripts/test.js
@@ -2,27 +2,43 @@
 var numberGuesses = 3;
 var randomNumber = Math.floor(Math.random() * 10) + 1;
 
+    
+    $( "#guessButton" ).click(function() {
+        var number = $("#guess").val();
+        if(guess < 0){
+            alert( "Number Please!" );
+        };
+    });
+     
+
 function compareNumber(){   
- var guess = document.getElementById("guess").value;
+    var guess = document.getElementById("guess").value;
+    var text=document.getElementById("message");
     if(guess > randomNumber){
-       console.log("Your guess is too high!");
+       
+        text.innerHTML="Too High!";
         numberGuesses = numberGuesses - 1;
 
         if(numberGuesses < 0){
            console.log("No More Guesses Left")
-        };
+            }
     }
     else if(guess < randomNumber){
-            console.log("Your guess is too low!");
+            text.innerHTML="Too Low!";
             numberGuesses = numberGuesses - 1;
         if(numberGuesses > 0){
            console.log("No More Guesses Left")
             }
-        }
+    }
     else{
         console.log("Your guess is correct!");   
     }  
     
     console.log(randomNumber); 
-};
-<!------>
+    };
+
+
+// add event listener
+var guessButton = document.getElementById("guessButton");
+guessButton.addEventListener("click", compareNumber, false);
+
