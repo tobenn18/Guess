7526b270d782c233e0a04ceff67bccff91a96b66
tobenn18 <bglumack@hotmail.com>
2015-07-24 14:20:17 -0500

    Validate etc

--- public/javascripts/test.js
+++ public/javascripts/test.js
@@ -2,43 +2,47 @@
 var numberGuesses = 3;
 var randomNumber = Math.floor(Math.random() * 10) + 1;
 
-    
-    $( "#guessButton" ).click(function() {
-        var number = $("#guess").val();
-        if(guess < 0){
+function isValid() {
+        var number = document.getElementById("guess").value;
+        if(number == null || number == "" || isNaN(number)){
             alert( "Number Please!" );
+            return false;
         };
-    });
-     
+       return true;
+    };  
 
-function compareNumber(){   
+function compareNumbers(){   
     var guess = document.getElementById("guess").value;
     var text=document.getElementById("message");
     if(guess > randomNumber){
-       
-        text.innerHTML="Too High!";
         numberGuesses = numberGuesses - 1;
-
+        text.innerHTML="Too High!" + "<br>" + "You have " + numberGuesses + " guesses left";
+       text.setAttribute("style", "font-size:24px;color:blue");
         if(numberGuesses < 0){
-           console.log("No More Guesses Left")
+            text.innerHTML="You have no more guesses left";
+           text.setAttribute("style", "font-size:36px;color:red");
             }
     }
     else if(guess < randomNumber){
-            text.innerHTML="Too Low!";
             numberGuesses = numberGuesses - 1;
+            text.innerHTML="Too Low!" + "<br>" + "You have " + numberGuesses + " guesses left";
+          text.setAttribute("style", "font-size:36px;color:green");
         if(numberGuesses > 0){
-           console.log("No More Guesses Left")
+            text.innerHTML="You have no more guesses left";
+            text.setAttribute("style", "font-size:36px;color:red");
             }
     }
     else{
-        console.log("Your guess is correct!");   
-    }  
-    
-    console.log(randomNumber); 
-    };
-
+         text.innerHTML="Your guess is correct!!!";
+      text.setAttribute("style", "font-size:58px;color:Purple");
+      }  
+     console.log(randomNumber);
+};
 
 // add event listener
 var guessButton = document.getElementById("guessButton");
-guessButton.addEventListener("click", compareNumber, false);
-
+guessButton.addEventListener("click", function () {
+  if (isValid()) {
+    compareNumbers();
+  }
+}, false);
